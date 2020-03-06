ba4786ae5cc5256b8860be38d2c6536df915388b
mayu.fukui <ma1927y@yahoo.co.jp>
2020-03-06 06:29:04 +0000

    kadai-tasklist add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_011021) do
+ActiveRecord::Schema.define(version: 2020_03_06_062258) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
